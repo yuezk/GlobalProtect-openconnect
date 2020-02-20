a3a1cf24d6cc546e6030e98c231d5b2539cb1200
Kevin Yue <yuezk001@gmail.com>
2020-02-20 20:42:51 +0800

    Add default port

--- GPClient/main.cpp
+++ GPClient/main.cpp
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
+    QString port = QString::fromLocal8Bit(qgetenv(ENV_CDP_PORT));
+    if (port == "") {
+        qputenv(ENV_CDP_PORT, "12315");
+    }
     SingleApplication app(argc, argv);
     GPClient w;
     w.show();
