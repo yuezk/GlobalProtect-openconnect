c58c2a45de1a14cffd6473dc9fa6b67e8105d648
Kevin Yue <yuezk001@gmail.com>
2021-08-21 08:16:31 +0800

    Set the gateway

--- GPClient/gatewayauthenticator.cpp
+++ GPClient/gatewayauthenticator.cpp
@@ -10,6 +10,7 @@ using namespace gpclient::helper;
 
 GatewayAuthenticator::GatewayAuthenticator(const QString& gateway, const PortalConfigResponse& portalConfig)
     : QObject()
+    , gateway(gateway)
     , preloginUrl("https://" + gateway + "/ssl-vpn/prelogin.esp?tmp=tmp&kerberos-support=yes&ipv6-support=yes&clientVer=4100&clientos=Linux")
     , loginUrl("https://" + gateway + "/ssl-vpn/login.esp")
     , portalConfig(portalConfig)
