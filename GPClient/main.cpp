98e641e99d0f25bf5d4f6b3a86256e9a4483181a
Kevin Yue <yuezk001@gmail.com>
2021-09-04 19:03:01 +0800

    release 1.3.3

--- GPClient/main.cpp
+++ GPClient/main.cpp
@@ -6,7 +6,7 @@
 #include <plog/Log.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
 
-static const QString version = "v1.3.2";
+static const QString version = "v1.3.3";
 
 int main(int argc, char *argv[])
 {
