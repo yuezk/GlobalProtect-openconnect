14b2f809b80bc4607cb1013dc4146c4aeb5666fd
Kevin Yue <yuezk001@gmail.com>
2021-09-13 23:58:51 +0800

    update dependency

--- cmake/Add3rdParty.cmake
+++ cmake/Add3rdParty.cmake
@@ -1,8 +1,8 @@
-include(cmake/CPM.cmake)
+include(ExternalProject)
 
-function(add_3rdparty)
-    cmake_parse_arguments(add_3rdparty_args "EXCLUDE_FROM_ALL" "NAME" "" ${ARGN})
-    set(NAME ${add_3rdparty_args_NAME})
+function(add_3rdparty NAME)
+    set(oneValueArgs GIT_REPOSITORY GIT_TAG)
+    cmake_parse_arguments(add_3rdparty_args "EXCLUDE_FROM_ALL" "${oneValueArgs}" "" ${ARGN})
 
     if(EXISTS "${CMAKE_SOURCE_DIR}/3rdparty/${NAME}/CMakeLists.txt")
         message(STATUS "Found third party locally for ${NAME}")
@@ -13,12 +13,21 @@ function(add_3rdparty)
             set(addSubdirectoryExtraArgs "")
         endif()
 
-        add_subdirectory(
-            "${CMAKE_SOURCE_DIR}/3rdparty/${NAME}"
+        ExternalProject_Add(
+            ${NAME}-${PROJECT_NAME}
+            PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${NAME}
+            SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/${NAME}"
+            INSTALL_COMMAND ""
             ${addSubdirectoryExtraArgs}
+            "${add_3rdparty_args_UNPARSED_ARGUMENTS}"
         )
         return()
     endif()
+
     message(STATUS "Using CPM to download ${NAME}") 
-    CPMAddPackage(${ARGN})
+    ExternalProject_Add(
+        ${NAME}-${PROJECT_NAME}
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${NAME}
+        "${ARGN}"
+    )
 endfunction()
