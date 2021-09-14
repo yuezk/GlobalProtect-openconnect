e012b536633ef51436b85f13c66f89c5b85ce84f
Kevin Yue <yuezk001@gmail.com>
2021-09-14 21:17:53 +0800

    remove QtCreator project file

--- cmake/Add3rdParty.cmake
+++ cmake/Add3rdParty.cmake
@@ -2,23 +2,16 @@ include(ExternalProject)
 
 function(add_3rdparty NAME)
     set(oneValueArgs GIT_REPOSITORY GIT_TAG)
-    cmake_parse_arguments(add_3rdparty_args "EXCLUDE_FROM_ALL" "${oneValueArgs}" "" ${ARGN})
+    cmake_parse_arguments(add_3rdparty_args "${oneValueArgs}" "" ${ARGN})
 
     if(EXISTS "${CMAKE_SOURCE_DIR}/3rdparty/${NAME}/CMakeLists.txt")
         message(STATUS "Found third party locally for ${NAME}")
-
-        if(${add_3rdparty_args_EXCLUDE_FROM_ALL})
-            set(addSubdirectoryExtraArgs EXCLUDE_FROM_ALL)
-        else()
-            set(addSubdirectoryExtraArgs "")
-        endif()
 
         ExternalProject_Add(
             ${NAME}-${PROJECT_NAME}
             PREFIX ${CMAKE_CURRENT_BINARY_DIR}/${NAME}
             SOURCE_DIR "${CMAKE_SOURCE_DIR}/3rdparty/${NAME}"
             INSTALL_COMMAND ""
-            ${addSubdirectoryExtraArgs}
             "${add_3rdparty_args_UNPARSED_ARGUMENTS}"
         )
         return()
