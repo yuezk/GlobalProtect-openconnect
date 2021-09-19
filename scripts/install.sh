74676c2e3a9a9e51814f7b27ae5c7180f8c7adee
Kevin Yue <yuezk001@gmail.com>
2021-09-19 14:17:35 +0800

    update message

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,4 @@ sudo ./cmakew --install build
 sudo systemctl daemon-reload
 sudo systemctl restart gpservice.service
 
-echo ""
-echo "Done. You can launch the GlobalProtect VPN client from the application dashboard."
-echo ""
+echo -e "\nDone. You can launch the GlobalProtect VPN client from the application dashboard.\n"
