2b11be942a636c972fe9aec18489128d7a331974
simon <simon.leary42@gmail.com>
2022-08-30 20:05:02 -0400

    copy install-ubuntu to install-debian

--- scripts/install-debian.sh
+++ scripts/install-debian.sh
@@ -1,1 +1,11 @@
-install-ubuntu.sh
+#!/bin/bash -e
+
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    qtbase5-dev \
+    libqt5websockets5-dev \
+    qtwebengine5-dev \
+    openconnect
+
+./scripts/install.sh
