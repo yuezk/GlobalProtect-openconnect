75a24c89cd977fa026db5cc801e010ab11cddfdc
simonleary-umass-edu <71396965+simonleary-umass-edu@users.noreply.github.com>
2022-08-31 04:28:11 -0400

    copy install script for debian (#180)
    Co-authored-by: simon <simon.leary42@gmail.com>

---
+++ scripts/install-debian.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    qtbase5-dev \
+    libqt5websockets5-dev \
+    qtwebengine5-dev \
+    openconnect
+
+./scripts/install.sh
