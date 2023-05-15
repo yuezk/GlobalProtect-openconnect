5a84c99cd92e4b91360ddd4af1796d523335256d
Kevin Yue <yuezk001@gmail.com>
2023-05-15 08:52:32 +0800

    refactor: support dark mode

--- gpgui/src/main.tsx
+++ gpgui/src/main.tsx
@@ -1,11 +1,35 @@
-import { CssBaseline } from '@mui/material'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useMemo } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <CssBaseline />
-    <App />
-  </React.StrictMode>,
-)
+function Root() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Root />
+);
