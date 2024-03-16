a00f6a8cba1e6cae33459379a425525771e0856b
Kevin Yue <yuezk001@gmail.com>
2024-03-16 12:05:09 +0800

    Add vpnc_script location, fix #336

--- crates/openconnect/src/vpnc_script.rs
+++ crates/openconnect/src/vpnc_script.rs
@@ -1,12 +1,13 @@
 use is_executable::IsExecutable;
 use std::path::Path;
 
-const VPNC_SCRIPT_LOCATIONS: [&str; 5] = [
+const VPNC_SCRIPT_LOCATIONS: [&str; 6] = [
   "/usr/local/share/vpnc-scripts/vpnc-script",
   "/usr/local/sbin/vpnc-script",
   "/usr/share/vpnc-scripts/vpnc-script",
   "/usr/sbin/vpnc-script",
   "/etc/vpnc/vpnc-script",
+  "/etc/openconnect/vpnc-script"
 ];
 
 pub(crate) fn find_default_vpnc_script() -> Option<String> {
