d94d730a446df63c6406c4e84635d737edcab744
Kevin Yue <k3vinyue@gmail.com>
2024-04-15 08:27:33 -0400

    feat: support default browser for CLI (#345)

--- crates/gpapi/src/process/mod.rs
+++ crates/gpapi/src/process/mod.rs
@@ -2,8 +2,6 @@ pub(crate) mod command_traits;
 pub(crate) mod gui_helper_launcher;
 
 pub mod auth_launcher;
-#[cfg(feature = "browser-auth")]
-pub mod browser_authenticator;
 pub mod gui_launcher;
 pub mod hip_launcher;
 pub mod service_launcher;
