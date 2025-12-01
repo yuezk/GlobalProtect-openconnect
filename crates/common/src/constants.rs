d6fa4ec9bbc80cc094b2efa565281a0544f7a476
Olivier Louvignes <olivier@mgcrea.io>
2025-12-01 14:26:33 +0100

    Add support for macOS builds (apple silicon)

--- crates/common/src/constants.rs
+++ crates/common/src/constants.rs
@@ -2,16 +2,28 @@ pub const GP_USER_AGENT: &str = "PAN GlobalProtect";
 pub const GP_SERVICE_LOCK_FILE: &str = "/var/run/gpservice.lock";
 pub const GP_CALLBACK_PORT_FILENAME: &str = "gpcallback.port";
 
-// Release binaries are hardcoded paths
-#[cfg(not(debug_assertions))]
+// Release binaries - macOS (Apple Silicon Homebrew)
+#[cfg(all(not(debug_assertions), target_os = "macos"))]
+pub const GP_CLIENT_BINARY: &str = "/opt/homebrew/bin/gpclient";
+#[cfg(all(not(debug_assertions), target_os = "macos"))]
+pub const GP_SERVICE_BINARY: &str = "/opt/homebrew/bin/gpservice";
+#[cfg(all(not(debug_assertions), target_os = "macos"))]
+pub const GP_GUI_BINARY: &str = "/opt/homebrew/bin/gpgui";
+#[cfg(all(not(debug_assertions), target_os = "macos"))]
+pub const GP_GUI_HELPER_BINARY: &str = "/opt/homebrew/bin/gpgui-helper";
+#[cfg(all(not(debug_assertions), target_os = "macos"))]
+pub const GP_AUTH_BINARY: &str = "/opt/homebrew/bin/gpauth";
+
+// Release binaries - Linux
+#[cfg(all(not(debug_assertions), not(target_os = "macos")))]
 pub const GP_CLIENT_BINARY: &str = "/usr/bin/gpclient";
-#[cfg(not(debug_assertions))]
+#[cfg(all(not(debug_assertions), not(target_os = "macos")))]
 pub const GP_SERVICE_BINARY: &str = "/usr/bin/gpservice";
-#[cfg(not(debug_assertions))]
+#[cfg(all(not(debug_assertions), not(target_os = "macos")))]
 pub const GP_GUI_BINARY: &str = "/usr/bin/gpgui";
-#[cfg(not(debug_assertions))]
+#[cfg(all(not(debug_assertions), not(target_os = "macos")))]
 pub const GP_GUI_HELPER_BINARY: &str = "/usr/bin/gpgui-helper";
-#[cfg(not(debug_assertions))]
+#[cfg(all(not(debug_assertions), not(target_os = "macos")))]
 pub const GP_AUTH_BINARY: &str = "/usr/bin/gpauth";
 
 // Debug binaries are set in build.rs via environment variables
